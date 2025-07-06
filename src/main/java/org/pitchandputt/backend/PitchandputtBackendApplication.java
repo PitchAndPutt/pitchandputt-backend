bf96ef57aca7d9cd73cafb57c8c328358ab4c072
Ashish Bhushan Kumar <abhushan064@gmail.com>
2025-07-06 17:44:52 +0530

    init: project scaffolding

---
+++ src/main/java/org/pitchandputt/backend/PitchandputtBackendApplication.java
@@ -0,0 +1,13 @@
+package org.pitchandputt.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitchandputtBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PitchandputtBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/pitchandputt/backend/PitchandputtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pitchandputt.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitchandputtBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
